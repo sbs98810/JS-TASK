6702e6f0136cd8aaafbdd703c3b40e62db880182
sbs98810 <101046744+sbs98810@users.noreply.github.com>
2022-04-09 11:17:01 +0530

    new

---
+++ level2/q3.js
@@ -0,0 +1,15 @@
+function min(a, b) {
+    if (a < b) {
+      return a;
+    } else {
+      return b;
+    }}
+    let ress=min(2,5)
+    console.log(ress)
+  
+/* 
+  function add(a,b="user"){
+    console.log(a+b)//30
+    return a+b}
+    let res=add("hello","john")
+*/
